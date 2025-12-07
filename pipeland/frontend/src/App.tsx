2b7fb49acd95fbf79325b24b1c4c07d2845a045a
Storm Søndergaard <storm@khaos.systems>
2025-12-08 00:36:29 +0100

    react flow

--- pipeland/frontend/src/App.tsx
+++ pipeland/frontend/src/App.tsx
@@ -1,9 +1,47 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
+import {useState, useCallback} from 'react';
 import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
-
+import { ReactFlow, applyNodeChanges, applyEdgeChanges, addEdge } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
+ 
+const initialNodes = [
+  { id: 'n1', position: { x: 0, y: 0 }, data: { label: 'Node 1' } },
+  { id: 'n2', position: { x: 0, y: 100 }, data: { label: 'Node 2' } },
+];
+const initialEdges = [{ id: 'n1-n2', source: 'n1', target: 'n2' }];
+ 
 function App() {
+  const [nodes, setNodes] = useState(initialNodes);
+  const [edges, setEdges] = useState(initialEdges);
+ 
+  const onNodesChange = useCallback(
+    (changes) => setNodes((nodesSnapshot) => applyNodeChanges(changes, nodesSnapshot)),
+    [],
+  );
+  const onEdgesChange = useCallback(
+    (changes) => setEdges((edgesSnapshot) => applyEdgeChanges(changes, edgesSnapshot)),
+    [],
+  );
+  const onConnect = useCallback(
+    (params) => setEdges((edgesSnapshot) => addEdge(params, edgesSnapshot)),
+    [],
+  );
+ 
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        colorMode='dark'
+        fitView
+      />
+    </div>
+  );
+}
+
+/*function App() {
     const [resultText, setResultText] = useState("Please enter your name below 👇");
     const [name, setName] = useState('');
     const updateName = (e: any) => setName(e.target.value);
@@ -23,6 +61,6 @@ function App() {
             </div>
         </div>
     )
-}
+}*/
 
 export default App
